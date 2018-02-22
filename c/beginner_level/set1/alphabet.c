bbc88ab0da6a0fdba58108955cdf850a8978692e
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 14:31:08 +0530

    Create alphabet.c

---
+++ c/beginner_level/set1/alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	char c;
+	scanf("%c",&c);
+	if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+	printf("alphapet");
+	else
+	printf("no");
+	return 0;
+}
+© 2018 GitHub, Inc.
