75604558e808dfee5241a0fa61dc40f2bdd05e43
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 15:29:37 +0530

    Update swap_bitwise.c

--- c/beginner_level/set6/swap_bitwise.c
+++ c/beginner_level/set6/swap_bitwise.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void) 
 {
- int i,k;
- scanf("%d %d",&i,&k);
-    printf(" value of i=%d k=%d before swapping", i, k);
-    i = i ^ k;
-    k = i ^ k;
-    i = i ^ k;
-    printf("\nvalue of i=%d k=%d after swapping", i, k);
+ int i,k1;
+ scanf("%d %d",&i,&k1);
+    printf(" value of i=%d k1=%d before swapping", i, k1);
+    i = i ^ k1;
+    k1 = i ^ k1;
+    i = i ^ k1;
+    printf("\nvalue of i=%d k1=%d after swapping", i, k1);
     return 0;
 }
