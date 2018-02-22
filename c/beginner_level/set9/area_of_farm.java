8c01e789d21fdcc6e14588a13f75a1f87cb64259
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 16:37:29 +0530

    Create area_of_farm.java

---
+++ c/beginner_level/set9/area_of_farm.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class fl{
+     public static void main(String aa[]){
+       Scanner s1=new Scanner(System.in);
+       float a=s1.nextFloat();
+       float b=s1.nextFloat();
+       float c=a*b;
+       
+       System.out.printf("%f",c);
+       
+}
+
+}
