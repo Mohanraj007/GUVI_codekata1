8ae355d130577599b71e44675eaaaf20164bc075
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 16:51:00 +0530

    Create 81.java

---
+++ c/beginner_level/set9/81.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class opp{
+
+     public static void main(String aa[]){
+     int a;
+     int b;
+     
+     int y;
+     
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+    
+       b=s.nextInt();
+  System.out.println(a-b);
+}
+}
