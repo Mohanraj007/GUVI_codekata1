b2ca9536c568a1b120c8960a565bc9633dc9ef70
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 15:01:37 +0530

    Create line_count.java

---
+++ c/beginner_level/set4/line_count.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String st;
+		int len;
+		Scanner s=new Scanner(System.in);
+		st=s.nextLine();
+		
+    String a[]=st.split(".");
+		
+	  System.out.println(a.length);
+	}
+}
