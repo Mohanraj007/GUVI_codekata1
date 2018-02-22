f8178cd39c46f8d687dc41c7a7e3684573d99c5d
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 16:06:57 +0530

    Update palindrome.c

--- c/beginner_level/set8/palindrome.c
+++ c/beginner_level/set8/palindrome.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int rev=0,t;
+int rev=0,t1;
 char a;
 printf("enter the string:");
 scanf("%s",&a);
-t=a;
-while(t!='\0')
+t1=a;
+while(t1!='\0')
 {
 rev=rev*10;
-rev=rev+t%10;
-t=t/10;
+rev=rev+t1%10;
+t1=t1/10;
 }
 if(a==rev)
 {
