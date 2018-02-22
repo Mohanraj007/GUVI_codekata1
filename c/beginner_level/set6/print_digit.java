1511857154490a77727eb6f0c364d03afdf75833
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 15:16:47 +0530

    Create print_digit.java

---
+++ c/beginner_level/set6/print_digit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String b;
+		Scanner sc=new Scanner(System.in);
+		b=sc.next();
+		
+		for(int i=0;i<b.length();i++)
+		{
+			System.out.print(b.charAt(i)+" ");
+		}
+	}
+}
