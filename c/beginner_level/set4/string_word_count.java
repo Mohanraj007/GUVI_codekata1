b5f6f9040afb5ddbe52ca597eab6c555d9c043ae
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 14:57:46 +0530

    Update string_word_count.java

--- c/beginner_level/set4/string_word_count.java
+++ c/beginner_level/set4/string_word_count.java
@@ -5,12 +5,12 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		String str;
+		String st;
 		int len;
 		Scanner s=new Scanner(System.in);
-		str=s.nextLine();
-		len=str.length();
-		String a[]=str.split(" ");
+		st=s.nextLine();
+		len=st.length();
+		String a[]=st.split(" ");
 		System.out.println(len-(a.length-1));
 		
 	}
