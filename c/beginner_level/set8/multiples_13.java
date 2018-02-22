2f53c6c7320358fa5a894ea661dc270a1d6b36fd
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 16:29:53 +0530

    Create multiples_13.java

---
+++ c/beginner_level/set8/multiples_13.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class po
+{
+     public static void main(String aa[]){
+       int b1;
+        Scanner s=new Scanner(System.in);
+       b1=s.nextInt();
+       if(b1%13==0){
+           System.out.println("Yes");
+       }
+ else{
+           System.out.println("No");
+ }
+ }
+}
