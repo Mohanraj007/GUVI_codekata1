dca044b4066f6d14cef5f5c97975d3755e0d26da
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 14:42:19 +0530

    Create count_digit.c

---
+++ c/beginner_level/set1/count_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+	int a,sum=0,r;
+	scanf("%d",&a);
+	while(a>0)
+	{
+   
+              a=a/10;
+              sum++;
+	}
+  printf("%d",sum);
+	
+	return 0;
+}
