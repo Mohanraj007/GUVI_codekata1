61215869bb8aa82eb84610b5b7b2997fda46f636
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-02-22 16:01:36 +0530

    Update differ_odd_even.c

--- c/beginner_level/set7/differ_odd_even.c
+++ c/beginner_level/set7/differ_odd_even.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main(void) {
-	int a,b;
-	scanf("%d %d",&a,&b);
-	a=a-b;
-	if(a%2==0)
+int main(void)
+{
+	int c,b;
+	scanf("%d %d",&c,&b);
+	c=c-b;
+	if(c%2==0)
 	printf("even");
 	else
 	printf("odd");
