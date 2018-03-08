64a963ca9f79f3d581a1df08ec14fbf3654b7cd3
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2018-03-08 12:03:52 +0530

    Update alphabet.c

--- c/beginner_level/set11/alphabet.c
+++ c/beginner_level/set11/alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void) 
 {
-	char a[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	char alph[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 	int n;
 	scanf("%d",&n);
-	printf("%c",a[n-1]);
+	printf("%c",alph[n-1]);
 	return 0;
 }
